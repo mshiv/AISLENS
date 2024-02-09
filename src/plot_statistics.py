e4afe4bfb654ec217a01943ee5ac6b07d89c20b7
Shivaprakash Muruganandham <mshiv@users.noreply.github.com>
2024-02-09 11:12:46 -0700

    Add function to clip map to a specified domain clip_data: This function is intended to abstract away the need to deal with rioxarray and geometry files User will only work with the names of the ice shelves, or regions of interest, provided in a list. Added documentation for remaining template functions, to be filled out.

--- src/plot_statistics.py
+++ src/plot_statistics.py
@@ -1,9 +1,15 @@
 # To plot exploratory statistics and maps of the raw and processed datasets
+# TODO: Convert to class and add more methods
 
 """
 Functions:
-1. scatter: Plot the dependence of melt rate on draft
-2. histogram: Plot the histogram of the melt rate
+0. clip_data: Clip the map to a specific domain (using rio.clip to set the domain mask)
+    Calling this function is optional. If the user does not call this function, the entire ice sheet will be plotted.
+    If the user calls this function, the map will be clipped to the specified domain.
+    It will need to be called before any of the other functions, when used.
+   
+1. scatter: Plot the dependence of melt rate on draft for a given ice shelf domain
+2. histogram: Plot the histogram of the melt rate for a given ice shelf domain
 3. time_series: Plot a spatially averaged time series of the melt rate over a defined domain (can be full ice sheet, or specific regions)
 4. power_spectrum: Plot the power spectrum of the melt rate time series
 5. autocorrelation: Plot the autocorrelation of the melt rate time series
@@ -18,6 +24,30 @@ Functions:
 14. map_kurtosis: Plot the spatial distribution of the kurtosis of the melt rate over a defined domain
 15. map_percentile: Plot the spatial distribution of the 95th percentile of the melt rate over a defined domain
 """
+# Import necessary libraries
+import numpy as np
+import xarray as xr
+import matplotlib.pyplot as plt
+from scipy import signal
+from shapely.geometry import mapping
+import geopandas as gpd
+import rioxarray
+
+# Load the ice shelf geometry
+
+def clip_data(data, domain):
+    """
+    Clip the map to a specific domain
+    data: input data (xarray DataArray)
+    domain: domain name (string), as defined in the ice shelf geometry file (icems)
+    """
+    # TODO: Include a step here to convert from domain name string to the domain index number used in the ice shelf geometry file
+    clipped_data = data.rio.clip(icems.loc[domain,'geometry'].apply(mapping))
+    #clipped_data = clipped_data.dropna('time',how='all')
+    #clipped_data = clipped_data.dropna('y',how='all')
+    #clipped_data = clipped_data.dropna('x',how='all')
+    #clipped_data = clipped_data.drop("month")
+    return clipped_data
 
 def scatter(melt, draft):
     """
