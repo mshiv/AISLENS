afb6d7c3406eb74615d618be05d8568d324c40e6
Shivaprakash Muruganandham <mshiv@users.noreply.github.com>
2025-06-02 04:02:48 -0700

    Update draft dependence workflows

--- src/scripts/calculate_draft_dependence.py
+++ src/scripts/calculate_draft_dependence.py
@@ -15,12 +15,9 @@
 #            using the fill_nan_with_nearest_neighbor_vectorized function.
 #   4. Save the extrapolated parameters to a specified output path.
 
-from aislens.dataprep import dedraft, setup_draft_depen_field
-from aislens.geospatial import find_ice_shelf_index, clip_data, process_ice_shelf
-from aislens.utils import fill_nan_with_nearest_neighbor_vectorized, initialize_directories, write_crs
+from aislens.dataprep import process_catchment
 from aislens.config import config
 import xarray as xr
-from shapely.geometry import mapping
 
 # Load the prepared satellite observation dataset
 satobs = xr.open_dataset(config.FILE_PAOLO23_SATOBS_PREPARED)
@@ -34,33 +31,15 @@ icems = xr.open_dataset(config.FILE_ICESHELFMASKS)
 # Loop through each ice shelf region defined in the configuration
 # Merge the dedrafted data across the entire ice sheet
 
-def process_catchment(i, icems, satobs, config):
-    print(f'Extracting data for catchment {icems.name.values[i]}')
-    ds = clip_data(satobs, i, icems)
-    ds_tm = ds.mean(dim='time')
-    
-    print(f'Calculating linear regression for catchment {icems.name.values[i]}')
-    mlt_coef, mlt_intercept = dedraft(ds.melt, ds.draft)
-    
-    # Retrieve attribute keys explicitly for clarity
-    alpha0_key, alpha1_key = list(config.DATA_ATTRS.keys())
-    mlt_coef_ds = setup_draft_depen_field(ds_tm.melt, mlt_coef, alpha1_key, i, icems)
-    mlt_intercept_ds = setup_draft_depen_field(ds_tm.melt, mlt_intercept, alpha0_key, i, icems)
-    
-    # Combine and save
-    mlt_coefs = xr.Dataset({
-        mlt_coef_ds.name: mlt_coef_ds,
-        mlt_intercept_ds.name: mlt_intercept_ds
-        })
-    mlt_coefs.to_netcdf(config.DIR_ICESHELF_DEDRAFT / f'draftDepenBasalMeltAlpha_{icems.name.values[i]}.nc')
-    print(f'{icems.name.values[i]} file saved')
-
 def calculate_draft_dependence(icems, satobs, config):
     for i in config.ICE_SHELF_REGIONS:
-        process_catchment(i, icems, satobs, config)
+        process_catchment(i, icems, satobs, config, 
+                          save_dir=config.DIR_ICESHELF_DEDRAFT_SATOBS,
+                          save_coefs=True
+                          )
     draft_dependence_params = xr.Dataset()
     for i in config.ICE_SHELF_REGIONS:
-        draft_dependence_params = xr.merge([draft_dependence_params, xr.open_dataset(config.DIR_ICESHELF_DEDRAFT / 'draftDepenBasalMeltAlpha_{}.nc'.format(icems.name.values[i]))])
+        draft_dependence_params = xr.merge([draft_dependence_params, xr.open_dataset(config.DIR_ICESHELF_DEDRAFT_SATOBS / 'draftDepenBasalMeltAlpha_{}.nc'.format(icems.name.values[i]))])
     # Extrapolate draft dependence parameters
     draft_dependence_params = draft_dependence_params.fillna(0)  # Fill NaN values with 0 before extrapolation
     # draft_dependence_extrapolated = fill_nan_with_nearest_neighbor_vectorized(draft_dependence_params)
