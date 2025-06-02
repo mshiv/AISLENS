593e524406933c59dadeef4fde4867a316eeef2a
Shivaprakash Muruganandham <mshiv@users.noreply.github.com>
2025-06-02 00:33:24 -0700

    WIP: Update main script with workflow steps

--- src/scripts/main.py
+++ src/scripts/main.py
@@ -9,21 +9,35 @@
 
 import subprocess
 from concurrent.futures import ThreadPoolExecutor
+from aislens.config import config
+from aislens.utils import collect_directories, initialize_directories
 
 def run_script(script_name):
     subprocess.run(["python", script_name])
 
 def main():
+    # Step 0: Initialize all directories
+    print("Initializing data directories...")
+    dirs_to_create = collect_directories(config)
+    initialize_directories(dirs_to_create)
+    print("DATA DIRECTORIES INITIALIZED SUCCESSFULLY.")
+
     # Step 1: Run prepare_data.py
+    print("Running prepare_data.py...")
     run_script("prepare_data.py")
+    print("prepare_data.py COMPLETED SUCCESSFULLY.")
     
     # Step 2 & 3: Run calculate_draft_dependence.py and generate_forcings.py in parallel
     with ThreadPoolExecutor() as executor:
+        print("Running calculate_draft_dependence.py and generate_forcings.py in parallel...")
         executor.submit(run_script, "calculate_draft_dependence.py")
         executor.submit(run_script, "generate_forcings.py")
-    
+        print("calculate_draft_dependence.py and generate_forcings.py COMPLETED SUCCESSFULLY.")
+
     # Step 4: Run regrid_to_mali.py
+    print("Running regrid_to_mali.py...")
     run_script("regrid_to_mali.py")
+    print("regrid_to_mali.py COMPLETED SUCCESSFULLY.")
 
 if __name__ == "__main__":
     main()
