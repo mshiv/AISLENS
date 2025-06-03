8733041401029733e1126de782dc14a34c348cf8
Shivaprakash Muruganandham <mshiv@users.noreply.github.com>
2025-06-02 19:06:01 -0700

    WIP: Update data variable name in dedraft modules

--- src/aislens/dataprep.py
+++ src/aislens/dataprep.py
@@ -131,11 +131,11 @@ def dedraft_catchment(
     ds = clip_data(data, i, icems)
     ds_tm = ds.mean(dim=config.TIME_DIM)
     # Choose the correct variable names based on the data type
-    if config.SORRM_FLUX_VAR==ds.name:
+    if config.SORRM_FLUX_VAR in ds.data_vars:
         # If the data is from the model, use SORRM variables
         flux_var = config.SORRM_FLUX_VAR
         draft_var = config.SORRM_DRAFT_VAR
-    elif config.SATOBS_FLUX_VAR==ds.name:
+    elif config.SATOBS_FLUX_VAR in ds.data_vars:
         # For satellite observations, use the SATOBS variables
         flux_var = config.SATOBS_FLUX_VAR
         draft_var = config.SATOBS_DRAFT_VAR
